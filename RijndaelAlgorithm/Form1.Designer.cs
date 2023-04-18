caadbd02d06099492f7aa4b218f94f45116376c1
abdolati <abd1983n@gmail.com>
2023-04-18 16:31:20 +0200

    Add project files.

---
+++ RijndaelAlgorithm/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace RijndaelAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtPathFileDecrypte = new System.Windows.Forms.TextBox();
+            this.TxtPathfilEncrypt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxPathFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btndecrypt = new System.Windows.Forms.Button();
+            this.TxtDecryptedFileTime = new System.Windows.Forms.TextBox();
+            this.TxtEncryptTime = new System.Windows.Forms.TextBox();
+            this.btnencrypt = new System.Windows.Forms.Button();
+            this.btnimportFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 362);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "DecryptPathFile";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 230);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "EncryptPathFile";
+            // 
+            // TxtPathFileDecrypte
+            // 
+            this.TxtPathFileDecrypte.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPathFileDecrypte.Location = new System.Drawing.Point(172, 352);
+            this.TxtPathFileDecrypte.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtPathFileDecrypte.Name = "TxtPathFileDecrypte";
+            this.TxtPathFileDecrypte.Size = new System.Drawing.Size(578, 31);
+            this.TxtPathFileDecrypte.TabIndex = 37;
+            // 
+            // TxtPathfilEncrypt
+            // 
+            this.TxtPathfilEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPathfilEncrypt.Location = new System.Drawing.Point(172, 221);
+            this.TxtPathfilEncrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtPathfilEncrypt.Name = "TxtPathfilEncrypt";
+            this.TxtPathfilEncrypt.Size = new System.Drawing.Size(578, 31);
+            this.TxtPathfilEncrypt.TabIndex = 36;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 120);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "PathFile";
+            // 
+            // TxPathFile
+            // 
+            this.TxPathFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxPathFile.Location = new System.Drawing.Point(136, 110);
+            this.TxPathFile.Margin = new System.Windows.Forms.Padding(4);
+            this.TxPathFile.Name = "TxPathFile";
+            this.TxPathFile.Size = new System.Drawing.Size(614, 31);
+            this.TxPathFile.TabIndex = 34;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 197);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "EncryptTime";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 323);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "DecryptTime";
+            // 
+            // btndecrypt
+            // 
+            this.btndecrypt.Location = new System.Drawing.Point(758, 353);
+            this.btndecrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.btndecrypt.Name = "btndecrypt";
+            this.btndecrypt.Size = new System.Drawing.Size(103, 35);
+            this.btndecrypt.TabIndex = 31;
+            this.btndecrypt.Text = "Decrypt";
+            this.btndecrypt.UseVisualStyleBackColor = true;
+            this.btndecrypt.Click += new System.EventHandler(this.btndecrypt_Click);
+            // 
+            // TxtDecryptedFileTime
+            // 
+            this.TxtDecryptedFileTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtDecryptedFileTime.Location = new System.Drawing.Point(172, 313);
+            this.TxtDecryptedFileTime.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtDecryptedFileTime.Name = "TxtDecryptedFileTime";
+            this.TxtDecryptedFileTime.Size = new System.Drawing.Size(215, 31);
+            this.TxtDecryptedFileTime.TabIndex = 30;
+            // 
+            // TxtEncryptTime
+            // 
+            this.TxtEncryptTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtEncryptTime.Location = new System.Drawing.Point(172, 182);
+            this.TxtEncryptTime.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtEncryptTime.Name = "TxtEncryptTime";
+            this.TxtEncryptTime.Size = new System.Drawing.Size(215, 31);
+            this.TxtEncryptTime.TabIndex = 29;
+            // 
+            // btnencrypt
+            // 
+            this.btnencrypt.Location = new System.Drawing.Point(758, 218);
+            this.btnencrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.btnencrypt.Name = "btnencrypt";
+            this.btnencrypt.Size = new System.Drawing.Size(103, 34);
+            this.btnencrypt.TabIndex = 28;
+            this.btnencrypt.Text = "Encrypt";
+            this.btnencrypt.UseVisualStyleBackColor = true;
+            this.btnencrypt.Click += new System.EventHandler(this.btnencrypt_Click);
+            // 
+            // btnimportFile
+            // 
+            this.btnimportFile.Location = new System.Drawing.Point(758, 110);
+            this.btnimportFile.Margin = new System.Windows.Forms.Padding(4);
+            this.btnimportFile.Name = "btnimportFile";
+            this.btnimportFile.Size = new System.Drawing.Size(103, 31);
+            this.btnimportFile.TabIndex = 27;
+            this.btnimportFile.Text = " ImportFile";
+            this.btnimportFile.UseVisualStyleBackColor = true;
+            this.btnimportFile.Click += new System.EventHandler(this.btnimportFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 499);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtPathFileDecrypte);
+            this.Controls.Add(this.TxtPathfilEncrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxPathFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btndecrypt);
+            this.Controls.Add(this.TxtDecryptedFileTime);
+            this.Controls.Add(this.TxtEncryptTime);
+            this.Controls.Add(this.btnencrypt);
+            this.Controls.Add(this.btnimportFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtPathFileDecrypte;
+        private System.Windows.Forms.TextBox TxtPathfilEncrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxPathFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btndecrypt;
+        private System.Windows.Forms.TextBox TxtDecryptedFileTime;
+        private System.Windows.Forms.TextBox TxtEncryptTime;
+        private System.Windows.Forms.Button btnencrypt;
+        private System.Windows.Forms.Button btnimportFile;
+    }
+}
+
---
+++ RijndaelAlgorithm/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RijndaelAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        private byte[] originalFileData;
+        private byte[] encryptedFileData;
+        private byte[] decryptedFileData;
+
+        private byte[] encryptionKey;
+        private byte[] encryptionIV;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnimportFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "All files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                originalFileData = File.ReadAllBytes(fileName);
+                TxPathFile.Text = fileName;
+            }
+        }
+
+        private void btnencrypt_Click(object sender, EventArgs e)
+        {
+            TimeSpan elapsedTime = TimeSpan.Zero;
+            DateTime start = DateTime.Now;
+
+            if (originalFileData == null)
+            {
+                MessageBox.Show("Please import a file first.");
+                return;
+            }
+
+            RijndaelManaged rijndael = new RijndaelManaged();
+            rijndael.GenerateKey();
+            rijndael.GenerateIV();
+
+            encryptionKey = rijndael.Key;
+            encryptionIV = rijndael.IV;
+
+            ICryptoTransform encryptor = rijndael.CreateEncryptor(encryptionKey, encryptionIV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    csEncrypt.Write(originalFileData, 0, originalFileData.Length);
+                    csEncrypt.FlushFinalBlock();
+                    encryptedFileData = msEncrypt.ToArray();
+                }
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Encrypted files (.enc)|.enc"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                File.WriteAllBytes(fileName, encryptedFileData);
+                TxtPathfilEncrypt.Text = fileName;
+            }
+
+            TimeSpan stopTime = DateTime.Now.Subtract(start);
+            elapsedTime += stopTime;
+            TxtEncryptTime.Text = elapsedTime.ToString();
+            //MessageBox.Show("File encryption complete. Time taken: " + elapsedTime.ToString());
+        }
+
+        private void btndecrypt_Click(object sender, EventArgs e)
+        {
+            TimeSpan elapsedTime = TimeSpan.Zero;
+            DateTime start = DateTime.Now;
+
+
+            if (encryptedFileData == null || encryptionKey == null || encryptionIV == null)
+            {
+                MessageBox.Show("Please encrypt a file first.");
+                return;
+            }
+
+            RijndaelManaged rijndael = new RijndaelManaged();
+            rijndael.Key = encryptionKey;
+            rijndael.IV = encryptionIV;
+
+            ICryptoTransform decryptor = rijndael.CreateDecryptor(rijndael.Key, rijndael.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream())
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
+                {
+                    csDecrypt.Write(encryptedFileData, 0, encryptedFileData.Length);
+                    csDecrypt.FlushFinalBlock();
+                    decryptedFileData = msDecrypt.ToArray();
+                }
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Original File Extension |" + Path.GetExtension(TxPathFile.Text)
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                File.WriteAllBytes(fileName, decryptedFileData);
+                TxtPathFileDecrypte.Text = fileName;
+            }
+
+            TimeSpan stopTime = DateTime.Now.Subtract(start);
+            elapsedTime += stopTime;
+            TxtDecryptedFileTime.Text = elapsedTime.ToString();
+
+            //MessageBox.Show("File decryption complete. Time taken: " + elapsedTime.ToString());
+        }
+    }
+}
